0460cb6d2f4f7e7e5528a99d25ef72b1b1cb5950
Sergey Udaltsov <40124345+SergeyUdaltsov@users.noreply.github.com>
2023-08-06 18:37:03 +0300

    Feature/create department (#82)
    * department creation finished
    
    * build fixed
    
    * python lambdas changed
    
    * config changed

--- admin/src/main/java/com/bot/lambda/AdminLambda.java
+++ admin/src/main/java/com/bot/lambda/AdminLambda.java
@@ -4,13 +4,11 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.bot.LambdaApplication;
-import com.bot.LambdaFunction;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-@LambdaFunction("adminLambda")
 public class AdminLambda implements RequestStreamHandler {
 
     public AdminLambda() throws ContainerInitializationException, IOException {
