2746befbf582dfc2b5e914a19efb267e1265c849
Sergey Udaltsov <40124345+SergeyUdaltsov@users.noreply.github.com>
2023-09-23 11:51:55 +0300

    localization bucket clean implemented (#160)

--- destroy.sh
+++ destroy.sh
@@ -4,11 +4,15 @@ source ${WORKSPACE}/deployment.config
 
 accountId="$1"
 uiBucket="appointer-ui-${accountId}"
+localizationBucket="appointer-localization-${accountId}"
 
 echo "Destroying the stack '${infraStack}'"
 
 echo "Cleaning ui bucket"
 aws s3 rm s3://${uiBucket} --recursive
+
+echo "Cleaning localization bucket"
+aws s3 rm s3://${localizationBucket} --recursive
 
 aws cloudformation delete-stack --stack-name ${infraStack} --region ${home_region}
 aws cloudformation wait stack-delete-complete --stack-name ${infraStack} --region ${home_region}
